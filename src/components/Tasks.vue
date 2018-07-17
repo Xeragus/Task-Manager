a162737917f0f4f83e72ed4cffdd0bf700369e9a
Xeragus <xeragus.code@gmail.com>
2018-07-17 20:07:30 +0200

    Adding app files

---
+++ src/components/Tasks.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <form @submit.prevent="addTask">
+        <input type="text" placeholder="Enter a task..." v-model="task" v-validate="'min:5'" name="task">
+        <p class="alert" v-if="errors.has('task')">{{ errors.first('task') }}</p>
+      </form>
+
+      <ul>
+        <transition-group name="list" enter-active-class="animate bounceInUp" leave-active-class="animate bounceOutDown">
+          <li v-for="(data, index) in tasks" :key="index">
+            {{ data.task }}
+            <i class="fa fa-minus-circle" v-on:click="removeTask(index)"></i>
+          </li>
+        </transition-group >
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tasks',
+  data() {
+    return {
+      task: '',
+      tasks: [
+        {"task": "Go to the grocery store"},
+        {"task": "Learn Node.js"},
+        {"task": "Read a book"}
+      ]
+    }
+  },
+  methods: {
+    addTask() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.tasks.push({ task: this.task });
+          this.task = '';
+        } else {
+          // eslint-disable-next-line
+          console.log("not valid");
+        }
+      })
+    },
+    removeTask(id) {
+      this.tasks.splice(id, 1);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"; 
+  .holder {
+    background: #fff;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #2297BD;
+    border-left: 5px solid #3E543D;
+    margin-bottom: 2px;
+    color: #fff;
+  }
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+  .alert {
+    background: #cb2222;
+    font-weight: bold;
+    display: inline-block;
+    width: 100%;
+    color: #fff;
+    margin: 0;
+  }
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+i {
+  float:right;
+  cursor:pointer;
+}
+</style>
